8b5ff1d9e629f46652aa432fd02294849787b84b
hyowonsong <woniwoni30@gmail.com>
2024-02-24 16:56:42 +0900

    [Add] User Service API 구현을 위해 UserController 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
@@ -1,2 +1,41 @@
-package kr.co.joneconsulting.myrestfulservice.Controller;public class UserController {
+package kr.co.joneconsulting.myrestfulservice.Controller;
+
+import kr.co.joneconsulting.myrestfulservice.bean.User;
+import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+
+    public UserController(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        return user;
+    }
+
+
+//    @PostMapping("/users")
+//    public void createUser(@RequestBody User user){
+//        User savedUser = service.save(user);
+//    }
+//    HTTP Status Code 를 제어하기 위해 아래와 같이 변형
+
+
 }
