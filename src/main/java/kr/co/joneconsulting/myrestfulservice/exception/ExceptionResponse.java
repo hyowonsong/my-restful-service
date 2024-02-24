b1c9d077a1b0a007e0695500315789811e10393f
hyowonsong <woniwoni30@gmail.com>
2024-02-24 20:31:41 +0900

    [Add] 일반화된 예외 클래스 만들기

--- src/main/java/kr/co/joneconsulting/myrestfulservice/exception/ExceptionResponse.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/exception/ExceptionResponse.java
@@ -1,2 +1,16 @@
-package kr.co.joneconsulting.myrestfulservice.exception;public class ExceptionResponse {
+package kr.co.joneconsulting.myrestfulservice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+    private Date timestamp;
+    private String message;
+    private String details;
 }
