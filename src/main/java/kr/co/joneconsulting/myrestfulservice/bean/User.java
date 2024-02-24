ce3b5d40c5f094e2a5c3f81b2e2db903e16e9c90
hyowonsong <woniwoni30@gmail.com>
2024-02-24 20:27:16 +0900

    [Update] Validation 관련 어노테이션 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/bean/User.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/bean/User.java
@@ -1,5 +1,7 @@
 package kr.co.joneconsulting.myrestfulservice.bean;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +10,12 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 public class User {
+
     private Integer id;
+
+    @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
     private String name;
+
+    @Past(message = "등록일은 미래 날짜를 입력할실 수 없습니다.")
     private Date joinDate;
 }
