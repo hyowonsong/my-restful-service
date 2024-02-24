0192e00b3a046eca9bafc4967e5a9bb5e481093d
hyowonsong <woniwoni30@gmail.com>
2024-02-24 16:38:33 +0900

    [Add] HelloWorldController 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/HelloWorldController.java
@@ -1,2 +1,22 @@
-package kr.co.joneconsulting.myrestfulservice.Controller;public class HelloWorldController {
+package kr.co.joneconsulting.myrestfulservice.Controller;
+
+import kr.co.joneconsulting.myrestfulservice.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+
+
 }
