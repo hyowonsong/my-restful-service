de26936a2b1efb636254be078cb22bcd39975a1d
hyowonsong <woniwoni30@gmail.com>
2024-02-24 16:41:20 +0900

    [Add] HellowWorldController에 가변 API @PathVariable 사용

--- src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/HelloWorldController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package kr.co.joneconsulting.myrestfulservice.Controller;
 
 import kr.co.joneconsulting.myrestfulservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path/variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 
