af3d91c6661f72644077d41da42e1910487a58de
hyowonsong <woniwoni30@gmail.com>
2024-02-24 20:28:03 +0900

    [Add] 사용자 요청을 처리할 수 있는 UserController 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
@@ -1,10 +1,14 @@
 package kr.co.joneconsulting.myrestfulservice.Controller;
 
+import jakarta.validation.Valid;
 import kr.co.joneconsulting.myrestfulservice.bean.User;
 import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
+import kr.co.joneconsulting.myrestfulservice.exception.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -37,5 +41,24 @@ public class UserController {
 //    }
 //    HTTP Status Code 를 제어하기 위해 아래와 같이 변형
 
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+        User savedUser = service.save(user);
 
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+
+        if(user==null){
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+    }
 }
