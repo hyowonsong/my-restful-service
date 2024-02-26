f700553bb9cf34d7a2d695d3af0eff97bb7cdf86
hyowonsong <woniwoni30@gmail.com>
2024-02-26 20:48:46 +0900

    [Update] 사용자 정보 조회 관련 내용 Swagger에 추가

--- src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
+++ src/main/java/kr/co/joneconsulting/myrestfulservice/Controller/UserController.java
@@ -1,5 +1,12 @@
 package kr.co.joneconsulting.myrestfulservice.Controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.joneconsulting.myrestfulservice.bean.User;
 import kr.co.joneconsulting.myrestfulservice.dao.UserDaoService;
@@ -17,6 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러")
 public class UserController {
     private UserDaoService service;
 
@@ -29,6 +37,15 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용하여 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = User.class)))),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!"),
+    })
+    
     @GetMapping("/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
